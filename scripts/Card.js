bc641042d9eb9a9c6fd3cf15d07a32b13ff3a777
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-05-02 19:10:32 +0300

    Add files via upload

---
+++ scripts/Card.js
@@ -0,0 +1,51 @@
+export class Card {
+    constructor (data, templateSelector, handleClick) {
+      this._title = data.nameCard;
+      this._link = data.linkCard;
+      this._templateSelector = templateSelector;
+      this._handleClick = handleClick;
+      }
+
+      _getTemplate() {
+        const elementCard = document
+          .querySelector(this._templateSelector)
+          .content
+          .querySelector('.element')
+          .cloneNode(true);
+
+        return elementCard;
+      }
+
+      _delete () {
+        this._elementList.closest('.element').remove();
+      }
+
+        _toggleLike() {
+          this._buttonLike.classList.toggle('element__like-btn_active')
+        };
+
+        _showImage(event) {
+          if(!event.target.classList.contains('element__delete')) {
+            this._handleClick(this._title, this._link)
+          }
+        };
+
+         _setEventListeners() {
+          this._elementDelete.addEventListener('click', () => { this._delete(); });
+          //this._elementLike.addEventListener('click', () => { this._toggleLike(); });
+          this._elementImg.addEventListener('click', event => {this._showImage(event); });
+        }
+
+         getCardElement() {
+          this._element = this._getTemplate();
+          this._elementLike = this._element.querySelector('.element__like');
+          this._elementImg = this._element.querySelector('.element__image');
+          this._elementDelete = this._element.querySelector('.element__delete');
+          this._elementText = this._element.querySelector('.element__title');
+          this._setEventListeners();
+
+          return this._element;
+
+           }
+
+          }
