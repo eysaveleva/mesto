852eb8685c075c310af4ce81679a0d4cce6b7c04
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-05-25 22:31:49 +0300

    Add files via upload

---
+++ components/Section.js
@@ -0,0 +1,19 @@
+
+export default class Section {
+    constructor ({ data, renderer }, selector) {
+      this._items = data;
+      this._renderer = renderer;
+
+      this._container = document.querySelector(selector);
+    }
+
+    renderItems() {
+      this._items.forEach((item) => {
+          this._renderer(item);
+      });
+  }
+
+    addItem(element) {
+      this._container.prepend(element);
+    }
+  }
