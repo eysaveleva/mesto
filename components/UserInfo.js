1e2b078d76394e03a4aae1511fef702a108ca92a
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-05-25 17:06:01 +0300

    Add files via upload

---
+++ components/UserInfo.js
@@ -0,0 +1,21 @@
+export default class UserInfo {
+    constructor({ profileTitle, profileSubtitle }) {
+      this._profileTitle = document.querySelector(profileTitle);
+      this._profileSubtitle = document.querySelector(profileSubtitle);
+    }
+
+  getUserInfo() {
+      this._userInfo = {
+        profileTitle: this._profileTitle.textContent,
+        profileSubtitle: this._profileSubtitle.textContent
+      };
+      return this._userInfo;
+  }
+
+  setUserInfo(data) {
+    console.log(data);
+    this._profileTitle.textContent = data.newname;
+    this._profileSubtitle.textContent = data.about;
+
+}
+  }
